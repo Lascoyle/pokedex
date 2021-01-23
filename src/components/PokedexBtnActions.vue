69438ea5dc095943e8b91e8ad45c1b23a516658c
Lascoyle <meursault45@live.com>
2021-01-23 17:54:15 +0100

    Add labels to options and action buttons

--- src/components/PokedexBtnActions.vue
+++ src/components/PokedexBtnActions.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="pokedex-btn-actions">
-      <button class="pokedex-btn-action cancel"></button>
-      <button class="pokedex-btn-action confirm"></button>
+      <div class="pokedex-btn-actions-group">
+          <button class="pokedex-btn-action cancel"></button>
+          <button class="pokedex-btn-action confirm"></button>
+      </div>
+      <div class="pokedex-btn-actions-labels">
+          <p>B</p>
+          <p>A</p>
+      </div>
   </div>
 </template>
 
@@ -13,13 +19,26 @@ export default {
 
 <style>
 #pokedex-btn-actions {
-    display: flex;
-    justify-content: space-between;
     width: 33%;
-    padding: 5px;
-    background-color: rgb(211, 63, 63);
     transform: rotate(-20deg);
+}
+
+.pokedex-btn-actions-group, .pokedex-btn-actions-labels {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
+
+.pokedex-btn-actions-group {
+    background-color: rgb(211, 63, 63);
+    padding: 5px;
     border-radius: 50px;
+}
+
+.pokedex-btn-actions-labels {
+    padding: 5px;
+    color: rgb(59, 58, 68);
+    font-weight: 600;
 }
 
 .pokedex-btn-action {
--- src/components/PokedexBtnOptions.vue
+++ src/components/PokedexBtnOptions.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="pokedex-btn-options">
-    <button class="pokedex-btn-option select"></button>
-    <button class="pokedex-btn-option start"></button>
+    <div id="pokedex-btn-options-group">
+      <button class="pokedex-btn-option select"></button>
+      <button class="pokedex-btn-option start"></button>
+    </div>
+    <div id="pokedex-btn-options-labels">
+      <p class="pokedex-btn-options-label">Select</p>
+      <p class="pokedex-btn-options-label">Start</p>
+    </div>
   </div>
 </template>
 
@@ -13,13 +19,29 @@ export default {
 
 <style>
 #pokedex-btn-options {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
   width: 30%;
   margin-left: 15px;
+  margin-bottom: -20px;
   align-self: flex-end;
+}
 
+#pokedex-btn-options-group, #pokedex-btn-options-labels {
+  display: flex;
+  justify-content: space-between;
+}
+
+#pokedex-btn-options-group {
+  margin-top: 10px;
+  margin-bottom: 2px ;
+}
+
+.pokedex-btn-options-label {
+  transform: rotate(-25deg);
+  color: rgb(59, 58, 68);
+  text-transform: uppercase;
+  font-size: 1.1rem;
+  margin-left: 10px;
+  font-weight: 600;
 }
 
 .pokedex-btn-option {
